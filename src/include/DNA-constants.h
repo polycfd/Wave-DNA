b66802c605c9c01637b6af1ae184dbc5035e7ce6
Fabian Denner <fabian.denner@gmail.com>
2023-03-10 10:20:14 +0100

    Removed ununsed macros

--- src/include/DNA-constants.h
+++ src/include/DNA-constants.h
@@ -6,12 +6,6 @@
 #define DNA_VERSION_NUM (0.31)
 static const char DNA_RELEASE_DATE[] = "17 Feb 2023";
 
-/**--------------------------------------------------------- 
-Not all of the following hash functions for pre-defined constants and
-operations are currently in use, but might be useful for later work.
-The header <DNA-constants.h> must be included to access the hash functions.
----------------------------------------------------------**/
-
 typedef double DNA_FLOAT;
 #define DNA_EPS (1.0e-15)
 #define DNA_SMALL (1.0e-30)
@@ -24,28 +18,11 @@ typedef double DNA_FLOAT;
 #define DNA_LOG(a) (log(a))
 #define DNA_CEIL(a) (ceil(a))
 #define DNA_STRINGTOFLOAT(a) (strtod(a, &dummy))
-
-/** Simple operations **/
 #define DNA_POW2(x) ((x) * (x))
 #define DNA_POW3(x) ((x) * (x) * (x))
-#define DNA_POW4(x) ((x) * (x) * (x) * (x))
-#define DNA_MAX(a, b) ((a) > (b) ? (a) : (b))
-#define DNA_MAX3(a, b, c) (((a) >= (b) && (a) >= (c)) ? (a) : ((b) >= (a) && (b) >= (c)) ? (b) : (c))
-#define DNA_MIN(a, b) ((a) < (b) ? (a) : (b))
-#define DNA_MIN3(a, b, c) (((a) <= (b) && (a) <= (c)) ? (a) : ((b) <= (a) && (b) <= (c)) ? (b) : (c))
-
-/** Pre-defined constants **/
-#define DNA_ONETHIRD (0.3333333333333333333333333333333333333333)
-#define DNA_TWOTHIRD (0.6666666666666666666666666666666666666667)
 #define DNA_PI (3.1415926535897932384626433832795028841972)
-#define DNA_E (2.71828182845904523536028747135266249775724709369995)
-
-/** Misc **/
-#define DNA_DATA_ALLOC_INCREMENT (10000)
 #define DNA_STRINGLENGTH (512)
-#define DNA_STRINGLENGTH_LONG (1024)
 #define DNA_STRINGLENGTH_SPRINTF (1024)
-#define DNA_STRINGLENGTH_SPRINTF_LONG (2048)
 
 /** Debugging aid **/
 #define DNA_WHERE printf("HERE - %s:%d\n", __FILE__, __LINE__);
