ec27f37d9eebb347724618b1b1c77b15e2f8a7fb
Fabian Denner <fabian.denner@gmail.com>
2023-03-10 10:06:17 +0100

    Corrections to the transformation between pressure and potential

--- src/transform/transformpotential.c
+++ src/transform/transformpotential.c
@@ -18,9 +18,7 @@ to the a0 coefficient of the time integration scheme).
 
 int TransformExcitationPressureToExcitationPotential(struct DNA_RunOptions *RunOptions, struct DNA_Fields *Fields, struct DNA_FluidProperties *FluidProperties)
 {
-  DNA_FLOAT NLflag = 0.0;  //RunOptions->LagrangianDensityFlag;
-
-  DNA_FLOAT phi;
+  DNA_FLOAT NLflag = (DNA_FLOAT) RunOptions->LagrangianDensityFlag;
 
   DNA_FLOAT p = RunOptions->pExcitation;
   DNA_FLOAT b = Fields->sum_aphi_dt1.val[RunOptions->iExcitation];
@@ -42,20 +40,15 @@ int TransformExcitationPressureToExcitationPotential(struct DNA_RunOptions *RunO
   DNA_FLOAT AG = u0 + 0.5 * (1.0 - DNA_POW2(u0) / pow2_c0) * transformed_dXI1_phi * NLflag;
   DNA_FLOAT N = (DNA_FLOAT) 0.5 * DNA_POW2(transformed_dt1) / pow2_c0 * NLflag;
 
-  phi = -b / a0 - dt / (a0 * A1) * ((A1 * q + AG * detJ) * dXI1_phi + N + p / rho0);
-
-  Fields->phi.val[RunOptions->iExcitation] = phi;
+  Fields->phi.val[RunOptions->iExcitation] = -b / a0 - dt / (a0 * A1) * ((A1 * q + AG * detJ) * dXI1_phi + N + p / rho0);
 
   return 0;
 }
 
 int TransformPotentialFieldToPressureField(struct DNA_RunOptions *RunOptions, struct DNA_Fields *Fields, struct DNA_FluidProperties *FluidProperties)
 {
-  DNA_FLOAT NLflag = RunOptions->LagrangianDensityFlag;
-
-  DNA_FLOAT p;
+  DNA_FLOAT NLflag = (DNA_FLOAT) RunOptions->LagrangianDensityFlag;
   DNA_FLOAT detJ = Fields->Grid.detJacobi;
-
   DNA_FLOAT rho0 = FluidProperties->rho0;
   DNA_FLOAT pow2_c0 = DNA_POW2(FluidProperties->c0);
 
@@ -71,11 +64,9 @@ int TransformPotentialFieldToPressureField(struct DNA_RunOptions *RunOptions, st
 
     DNA_FLOAT A1 = 1.0 - (transformed_dt1 + u0 * transformed_dXI1_phi) / pow2_c0 * NLflag;
     DNA_FLOAT AG = u0 + 0.5 * (1.0 - DNA_POW2(u0) / pow2_c0) * transformed_dXI1_phi * NLflag;
-    DNA_FLOAT N = (DNA_FLOAT) 0.5 * DNA_POW2(transformed_dt1) / pow2_c0 * NLflag;
+    DNA_FLOAT N = 0.5 * DNA_POW2(transformed_dt1) / pow2_c0 * NLflag;
 
-    p = -rho0 * (A1 * dtphi + (A1 * q + AG * detJ) * dXI1_phi + N);
-
-    Fields->PressureField.val[iPoint] = p;
+    Fields->PressureField.val[iPoint] = -rho0 * (A1 * dtphi + (A1 * q + AG * detJ) * dXI1_phi + N);
   }
 
   Fields->PressureField.val[RunOptions->iExcitation] = RunOptions->pExcitation;
