c291c48f650911a92a9dae5b17ae889adf4eb5ce
Fabian Denner <fabian.denner@gmail.com>
2023-03-12 14:56:06 +0100

    Corrections to CMakeLists.txt and build scripts.

--- build/cleanbuild_debug.sh
+++ build/cleanbuild_debug.sh
@@ -1,4 +1,3 @@
-#!/bin/tcsh
 rm -r CMakeCache.txt CMakeFiles Makefile WaveDNA cmake_install.cmake
 cmake CMakeLists.txt -DCMAKE_BUILD_TYPE=Debug
 make
--- build/cleanbuild_release.sh
+++ build/cleanbuild_release.sh
@@ -1,4 +1,3 @@
-#!/bin/tcsh
-rm -r CMakeCache.txt CMakeFiles Makefile WaveDNA cmake_install.cmake cmake.run.macosx.x86_64
+rm -r CMakeCache.txt CMakeFiles Makefile WaveDNA cmake_install.cmake
 cmake CMakeLists.txt -DCMAKE_BUILD_TYPE=Release
 make
