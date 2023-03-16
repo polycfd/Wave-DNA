375340818149663ce47d6d771f79426df802d493
SoerenSchenke <soeren.schenke@gmail.com>
2023-03-16 15:41:25 +0100

    removed analytical solutions from the postprocessing scripts plotHorizon.py in the ABH/AWH examples since this is unplublished information

--- examples/AcousticBlackWhiteHoles/AWH_rh1.5/plotHorizon.py
+++ examples/AcousticBlackWhiteHoles/AWH_rh1.5/plotHorizon.py
@@ -26,59 +26,6 @@ def _read(filename):
     fo.close()
     return t,p
 
-def _ABH_analytical_O1(t,He):
-    phorizon = []
-    thorizon = []
-    for i in range(0,len(t)):
-        thorizon.append(t[i])
-        phorizon.append((He/(He + 1.0*(t[i] - t[0])))**1.0)
-    return thorizon, phorizon
-
-def _AWH_analytical_O1(t,He):
-    phorizon = []
-    thorizon = []
-    for i in range(0,len(t)):
-        thorizon.append(t[i])
-        phorizon.append((He/(He - 1.0*(t[i] - t[0])))**1.0)
-    return thorizon, phorizon
-
-def _ABH_analytical_O2(t,rh,la,fa):
-    phorizon = []
-    thorizon = []
-    for i in range(0,len(t)):
-        ti=(t[i]-t[0])/fa
-        thorizon.append(t[i])
-        u1 = 2.0*c0/rh
-        u2 = -6.0*c0/rh**2
-        u3 = 24.0*c0/rh**3
-        u4 = -120.0*c0/rh**4
-        l = la + (u1*la + u3*la**3/24)*ti - 0.5*c0*(u2*la + u4*la**3/24)*ti**2
-        A1 = 2.0/rh
-        A2 = -2.0/rh**2
-        A3 = 4.0/rh**3
-        amplitude = ((A1*la**2 + A3/24.0*la**4)/(A1*l**2 + A3/24.0*l**4))**0.5
-        phorizon.append(amplitude)
-    return thorizon, phorizon
-
-def _AWH_analytical_O2(t,rh,la,fa):
-    phorizon = []
-    thorizon = []
-    for i in range(0,len(t)):
-        ti=(t[i]-t[0])/fa
-        thorizon.append(t[i])
-        u1 = -2.0*c0/rh
-        u2 = 6.0*c0/rh**2
-        u3 = -24.0*c0/rh**3
-        u4 = 120.0*c0/rh**4
-        l = la + (u1*la - u3*la**3/24)*ti + 0.5*c0*(u2*la + u4*la**3/24)*ti**2
-        A1 = 2.0/rh
-        A2 = -2.0/rh**2
-        A3 = 4.0/rh**3
-        amplitude = ((A1*la**2 + A3/24.0*la**4)/(A1*l**2 + A3/24.0*l**4))**0.5
-        phorizon.append(amplitude)
-    return thorizon, phorizon
-
-
 
 optionsfile = 'run.DNA' # default if no command line option (plot *.py optionsfile) is given
 if len(sys.argv) > 1:
@@ -106,12 +53,6 @@ He = c0**2/(ah*la) # Helmhotz number
 filename = "results/horizon.dat"
 t,p = _read(filename)
 plt.plot(t,p, linewidth=2.0, color='black', linestyle='solid', label=r'$\mathrm{simulation\:linear\:wave}$')
-
-t,p = _AWH_analytical_O1(t,He)
-plt.plot(t,p, linewidth=2.0, color='blue', linestyle='dotted', label=r'$\mathrm{analytical\:linear\:wave}\:\mathcal{O}\left(t^1\right)$')
-
-t,p = _AWH_analytical_O2(t,rh,la,fa)
-plt.plot(t,p, linewidth=2.0, color='blue', linestyle='dashed', label=r'$\mathrm{analytical\:linear\:wave}\:\mathcal{O}\left(t^2\right)$')
 
 
 plt.xlabel('$f_{\mathrm{a}}t$', fontsize=fontsize)
